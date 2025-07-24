380d6970bb7a425084706e69d9b27e336a9ceb82
Bekbolot Zhaliev <bekbolot.zhaliev@gmail.com>
2025-07-24 14:44:30 -0500

    Fix banner image routing

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -13,7 +13,7 @@ const page = () => {
   return (
     <>
       <NewNavBar cP="menu" />
-      <BannerText title="Our Menu" image="/assets/Menubanner.webp" />
+      <BannerText title="Our Menu" image="/assets/MenuBanner.webp" />
       <Menu
         href={
           "https://slicelife.com/restaurants/il/chicago/60625/arzan-cafe/menu"
